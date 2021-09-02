9fbd254cad4e06046468f5c39d03dc816c9ccee1
mark novelero <mark.novelero@gmail.com>
2021-09-02 16:01:33 -0400

    deploy

--- src/fixed_header/FixedHeader.js
+++ src/fixed_header/FixedHeader.js
@@ -12,7 +12,6 @@ const FixedHeader = (props) => {
           <div className = "nav-div">
             <Link to = '/'><a onClick = {() => props.changeHome()}>Fresh Page</a></Link>
             <Link to = '/create'><a className = "choice" href="#">Create New Story</a></Link>
-            <Link to = "/userhome"><a className = "choice" href="#">User Collection</a></Link>
             <Link to = "/sitecollection"> <a className = "choice" href="#">Site Collection</a></Link>
           </div>
         </nav>
